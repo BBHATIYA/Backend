66ae2a4b1b1b35e113e80fb10be6645204bc7005
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2023-01-29 14:49:35 +0200

    Routes

--- Express/server.js
+++ Express/server.js
@@ -6,6 +6,20 @@ app.get("/", function (req, res) {
   res.send("<h1>Hello from server</h1>");
 });
 
+app.get("/contact", function (req, res) {
+  res.send("Contact Bhavesh@gmail.com");
+});
+
+app.get("/about", function (req, res) {
+  res.send(
+    "My name is Bhavesh and I live in berrsheva and work as full stack developer"
+  );
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("Cricket, reading");
+});
+
 app.listen(3002, function () {
   console.log("Server running on port 3002");
 });
