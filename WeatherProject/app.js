63ba1dea201e234800cc25842ba5327cfc763eb1
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2023-01-31 15:30:58 +0200

    wether app

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=3a5928f4cdf32e13973209a3eced314c&units=metric";
-  https.get(url, function (response) {
-    console.log(response.statusCode);
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const apiKey = "3a5928f4cdf32e13973209a3eced314c";
+  const units = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
+  https.get(url, function (response) {
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
@@ -16,7 +24,7 @@ app.get("/", function (req, res) {
       imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
       const weatherDescription = weatherData.weather[0].description;
       res.write(`<p>The weather Description is ${weatherDescription} </p>`);
-      res.write(`<h1>The temperature in London is ${temp} degrees</h1> `);
+      res.write(`<h1>The temperature in ${query} is ${temp} degrees</h1> `);
       res.write(`<img src = ${imageUrl}>`);
       res.send();
     });
