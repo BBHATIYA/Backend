db48d4075a3cef49c1d0839a62ce292c546c66c5
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2023-01-31 12:36:58 +0200

    BMI calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -4,6 +4,19 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+
+  let bmi = weight / (height * height);
+
+  res.send(`your BMI is ${bmi}`);
+});
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -14,7 +27,7 @@ app.post("/", function (req, res) {
 
   let result = num1 + num2;
 
-  res.send(`The result of total nubmers are ${result}`);
+  res.send(`The result of total numbers are ${result}`);
 });
 
 app.listen(3001, function () {
