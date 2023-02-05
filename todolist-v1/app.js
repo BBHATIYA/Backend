867e6a3237ce735c24b5bd1f53a64ae7f512ca7c
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2023-02-05 12:23:13 +0200

    todo app

---
+++ todolist-v1/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    let today = new Date();
+    let currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.send("Yey it's weekend");
+  } else res.send("Boo!! I have to work!");
+});
+
+app.listen(3001, function () {
+  console.log("server is running on 3001");
+});
