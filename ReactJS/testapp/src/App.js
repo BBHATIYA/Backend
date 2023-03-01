a07525ff3571d25bff269e99349c255f7487ab9c
BBHATIYA <BHATIYABHAVESH03@GMAIL.COM>
2023-03-01 11:14:29 +0200

    react calss compo

--- ReactJS/testapp/src/App.js
+++ ReactJS/testapp/src/App.js
@@ -1,46 +1,85 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import BlogItem from "./BlogCard";
 import { isArrayEmpty } from "./Utils";
 import "./App.css";
 
-function App() {
-  const blogArr = [
-    {
-      id: 1,
-      title: "Blog Title 1",
-      Description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
-    },
-    {
-      id: 2,
-      title: "Blog Title 2",
-      Description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
-    },
-    {
-      id: 3,
-      title: "Blog Title 2",
-      Description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
-    },
-  ];
+class App extends Component {
+  state = {
+    showBlog: true,
+    blogArr: [
+      {
+        id: 1,
+        title: "Blog Title 1",
+        Description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        likeCount: 0,
+      },
+      {
+        id: 2,
+        title: "Blog Title 2",
+        Description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        likeCount: 0,
+      },
+      {
+        id: 3,
+        title: "Blog Title 2",
+        Description: "Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.",
+        likeCount: 0,
+      },
+    ],
+  };
 
-  const blogCards = isArrayEmpty(blogArr)
-    ? []
-    : blogArr.map((item, pos) => {
-        console.log(item);
-        return (
-          <BlogItem
-            key={pos}
-            className={"Blog"}
-            title={item.title}
-            description={item.Description}
-          />
-          // <div className="BlogCard" key={item.id}>
-          //   <h3>{item.title}</h3>
-          //   <p>{item.Description}</p>
-          // </div>
-        );
-      });
+  onLikeBtnClick = (pos) => {
+    const updatedBlogList = this.state.blogArr;
+    const updatedBlogObj = updatedBlogList[pos];
+    updatedBlogObj.likeCount = updatedBlogObj.likeCount + 1;
+    updatedBlogList[pos] = updatedBlogObj;
 
-  return <div className="App">{blogCards}</div>;
+    this.setState({ blogArr: updatedBlogList });
+  };
+
+  onHideBtnClick = () => {
+    let updatedState = !this.state.showBlog;
+    this.setState((prevState, prevProps) => {
+      return { showBlog: !prevState.showBlog };
+    });
+    console.log(this.showBlog);
+  };
+
+  render() {
+    console.log("Render Called");
+    console.log(this.state);
+
+    const blogCards = isArrayEmpty(this.state.blogArr)
+      ? []
+      : this.state.blogArr.map((item, pos) => {
+          console.log(item);
+          return (
+            <BlogItem
+              key={pos}
+              className={"Blog"}
+              title={item.title}
+              likeCount={item.likeCount}
+              description={item.Description}
+              onLikeBtnClick={() => this.onLikeBtnClick(pos)}
+            />
+            // <div className="BlogCard" key={item.id}>
+            //   <h3>{item.title}</h3>
+            //   <p>{item.Description}</p>
+            // </div>
+          );
+        });
+
+    return (
+      <div className="App">
+        <button onClick={this.onHideBtnClick}>
+          {this.state.showBlog ? "Hide List" : "Show List"}
+        </button>
+        <br></br>
+        {this.state.showBlog ? blogCards : null}
+      </div>
+    );
+  }
 }
 
 export default App;
--- ReactJS/testapp/src/BlogCard.js
+++ ReactJS/testapp/src/BlogCard.js
@@ -1,15 +1,25 @@
-// import React from "react";
+import React from "react";
 
 import { dumpLogs } from "./Utils";
 import "./Blogcard.css";
 import classes from "./BlogCard.module.css";
 
 const BlogCard = (props) => {
+  // onLikeBtnClick = () => {
+  //   setState((prevState, prevProps) => {
+  //     return { likeCount: prevState.likeCount + 1 };
+  //   });
+  // };
+
   dumpLogs(props);
   return (
     <div className={classes.BlogCard}>
-      <h3>Blog Title</h3>
-      <p>Blog Description</p>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <p>
+        Like Count: <span className={classes.LikeCount}>{props.likeCount}</span>
+      </p>
+      <button onClick={props.onLikeBtnClick}>Like</button>
     </div>
   );
 };
